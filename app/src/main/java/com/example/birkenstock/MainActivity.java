d38441d7b902954deb281549388381ac81064bff
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-02-18 17:55:43 -0800

    Initial commit

---
+++ app/src/main/java/com/example/birkenstock/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.birkenstock;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content view to a different layout file
+        setContentView(R.layout.activity_cart); // Make sure this is the layout you want
+    }
+
+}
